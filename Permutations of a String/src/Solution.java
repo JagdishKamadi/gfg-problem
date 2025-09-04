ed74a88cf2bcf03d8d2b89059a6e34a5692d4411
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2025-09-04 20:41:45 +0530

    Updated the solution for Permutations of a String problem.

---
+++ Permutations of a String/src/Solution.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+
+    public static void main(String[] args) {
+        String str = "ABCB";
+        System.out.println(new Solution().findPermutation(str));
+    }
+
+    public ArrayList<String> findPermutation(String s) {
+        Set<String> set = new HashSet<>();
+        genPerm(s, "", set);
+        return new ArrayList<>(set);
+    }
+
+    private static void genPerm(String str, String res, Set<String> set) {
+        if (str.isEmpty()) {
+            set.add(res);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            String leftPart = str.substring(0, i);
+            String rightPart = str.substring(i + 1);
+            String fullPart = leftPart + rightPart;
+            genPerm(fullPart, res + str.charAt(i), set);
+        }
+    }
+}
